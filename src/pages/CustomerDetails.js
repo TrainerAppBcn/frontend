66dd76c5111783fd21643d221725190b1978b74d
Andreu Martínez <martinez.andreu@gmail.com>
2021-01-24 22:52:42 +0100

    version 20210124 with improvements on customer details

--- src/pages/CustomerDetails.js
+++ src/pages/CustomerDetails.js
@@ -1,8 +1,9 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import {TrainerContext} from "../contexts/TrainerContext";
 
 export default function CustomerDetails() {
+
     const { id } = useParams(); // It grabs the id parameter defined on the route.
     const {customersList, setCustomersList} = useContext(TrainerContext);
     let customerData, indexData, error;
@@ -11,9 +12,13 @@ export default function CustomerDetails() {
         customerData = customersList.find(customer => customer._id === id);
         indexData = customersList.findIndex(customer => customer._id === id);
         console.log("Customer Data: ", customerData);
+        const {name, surname} = customerData;
     } else {
         error = `The customer with id: ${id} didn't exist `;
     };
+
+    // const [name, setName] = useState(customerData.name);
+    // const [surname, setSurname] = useState(customerData.surname);
 
     // function handleChange(event, index) {
     //     const { name, value } = event.target;
@@ -25,90 +30,112 @@ export default function CustomerDetails() {
     //     setExpenses(newExpenses);
     // };
 
-    function handleChange() {
-        // const { name, value } = event.target; 
+    function handleChange(event) {
+        const { name, value } = event.target; 
+        console.log("Name: ", name);
+        console.log("Value: ", value);
+        console.log(typeof value);
         let newCustomerList = [...customersList];
-        newCustomerList.splice(indexData, 1, customerData);
+        if (name === 'weigth' || name === 'heigth') {
+            newCustomerList[indexData][name] = Number(value);
+        } else {
+            newCustomerList[indexData][name] = value; 
+        }
         setCustomersList(newCustomerList);
     };
 
+    function handleSubmit (event) {
+        event.preventDefault();
+
+    }
+
     return (
         <div>
-            <div className="custdet">
-                <h1 >Customer Details - { id }</h1>
+            <div className="custdata">
+                <h2>Customer Details - { id }</h2>
                 { error && <div> { error } </div>}
-                { customerData && (
-                    <article className="custdata">
+                { customersList && (
+                    <form onSubmit={handleSubmit}>
+                        <label>Name: </label>
                         <input
                             key="1"
-                            name="customerName"
-                            onChange={() => {
-                            handleChange();
-                            }}
+                            type="text"
+                            required
+                            name="name"
+                            onChange={(event) => handleChange(event)}
                             // onKeyDown={handleKeyDown}
-                            value={customerData.name}
+                            value={customersList[indexData].name}
                         />
+                        <label>Surname: </label>
                         <input
                             key="2"
-                            name="customerSurname"
-                            onChange={() => {
-                            handleChange();
-                            }}
+                            type="text"
+                            required
+                            name="surname"
+                            onChange={(event) => handleChange(event)}
                             // onKeyDown={handleKeyDown}
-                            value={customerData.surname}
+                            value={customersList[indexData].surname}
                         />
+                        <label>Weigth: </label>
                         <input
                             key="3"
                             type="number"
-                            name="customerWeigth"
-                            onChange={() => {
-                            handleChange();
-                            }}
+                            name="weigth"
+                            onChange={(event) => handleChange(event)}
                             // onKeyDown={handleKeyDown}
-                            value={customerData.weigth}
+                            value={customersList[indexData].weigth}
                         />
+                        <label>Heigth: </label>
                         <input
                             key="4"
                             type="number"
-                            name="customerHeigth"
-                            onChange={() => {
-                            handleChange();
-                            }}
+                            name="heigth"
+                            onChange={(event) => handleChange(event)}
                             // onKeyDown={handleKeyDown}
-                            value={customerData.heigth}
+                            value={customersList[indexData].heigth}
                         />
+                        <label>Birthdate: </label>
                         <input
                             key="5"
                             type="date"
-                            name="customerBirthdate"
-                            onChange={() => {
-                            handleChange();
-                            }}
+                            name="birthdate"
+                            onChange={(event) => handleChange(event)}
                             // onKeyDown={handleKeyDown}
-                            value={customerData.birthdate}
+                            value={customersList[indexData].birthdate}
                         />
+                        <label>Perímeters: </label>
+                        <select>
+                            <option value="Chest">Chest</option>
+                            <option value="Waist">Waist</option>
+                            <option value="Hip">Hip</option>
+                        </select>
+                        <label>Skin turgor: </label>
+                        <select>
+                            <option value="Chest">Legs</option>
+                            <option value="Waist">Arms</option>
+                            <option value="Hip">Waist</option>
+                        </select>
+                        <label>Objective: </label>
                         <input
                             key="6"
-                            name="customerObjective"
-                            onChange={() => {
-                            handleChange();
-                            }}
+                            name="objective"
+                            onChange={(event) => handleChange(event)}
                             // onKeyDown={handleKeyDown}
-                            value={customerData.objective}
+                            value={customersList[indexData].objective}
                         />
+                        <label>Injuries and/or disseases: </label>
                         <input
                             key="7"
-                            name="customerInjDis"
-                            onChange={() => {
-                            handleChange();
-                            }}
+                            name="injuriesDiseases"
+                            onChange={(event) => handleChange(event)}
                             // onKeyDown={handleKeyDown}
-                            value={customerData.injuriesDiseases}
+                            value={customersList[indexData].injuriesDiseases}
                         />
-                    </article>
+                        <button>Update Customer</button>
+                    </form>
                 )}
-                <p>It lacks perimeters</p>
-                <p>It lacks skin turgor</p>
+                <p>It lacks perimeters date with possibilities to add measurements on different dates</p>
+                <p>It lacks skin turgor date with possibilities to add measurements on different dates</p>
                 <p>It lacks going to look for trainer name</p>
             </div>          
         </div>
